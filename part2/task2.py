4560aac815f0d5ac86a21f8b97e7f740586a43bd
Andrey Preobrazhenskiy <andr163rus@inbox.ru>
2025-04-02 02:39:30 +0400

    + save_frequencies and create_mapping functions in task2

--- part2/task2.py
+++ part2/task2.py
@@ -26,3 +26,28 @@ def calculate_frequencies(text: str) -> Dict[str, float]:
     freq = Counter(text)
     total_chars = sum(freq.values())
     return {char: count / total_chars for char, count in freq.items()}
+
+
+def save_frequencies(freq_dict: Dict[str, float], filename: str) -> None:
+    """
+    Saves character frequency analysis to a JSON file.
+
+    :param freq_dict: Dictionary containing character frequencies.
+    :param filename: The path to the JSON file to save data.
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(freq_dict, file, ensure_ascii=False, indent=2)
+
+
+def create_mapping(cipher_freq: Dict[str, float], reference_freq: Dict[str, float]) -> Dict[str, str]:
+    """
+    Creates a mapping between cipher text characters and reference frequencies.
+
+    :param cipher_freq: Frequency dictionary from the cipher text.
+    :param reference_freq: Reference frequency dictionary.
+    :return: A dictionary mapping cipher characters to reference characters.
+    """
+    sorted_cipher_freq = sorted(cipher_freq.items(), key=lambda item: item[1], reverse=True)
+    sorted_reference_freq = sorted(reference_freq.items(), key=lambda item: item[1], reverse=True)
+
+    return {cipher_char: ref_char for (cipher_char, _), (ref_char, _) in zip(sorted_cipher_freq, sorted_reference_freq)}
