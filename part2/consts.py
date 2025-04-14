1d292fa998638258a9fa28279f91cd4cd1ef9552
Andrey Preobrazhenskiy <andr163rus@inbox.ru>
2025-04-14 15:44:41 +0400

    + file with consts (pi and paths)

---
+++ part2/consts.py
@@ -0,0 +1,10 @@
+cpp_sequence_txt = "C:\\Users\\Я\\Desktop\\isblab2\\part1\\c_gen\\cpp_sequence.txt"
+python_sequence_txt = "C:\\Users\\Я\\Desktop\\isblab2\\part1\\python_gen\\python_sequence.txt"
+java_sequence_txt = "C:\\Users\\Я\\Desktop\\isblab2\\part1\\java_gen\\java_sequence.txt"
+
+test_results_cpp = "test_results_cpp.txt"
+test_results_python = "test_results_python.txt"
+test_results_java = "test_results_java.txt"
+
+# Probabilities for block_run_test
+PI = [0.2148, 0.3672, 0.2305, 0.1875]
