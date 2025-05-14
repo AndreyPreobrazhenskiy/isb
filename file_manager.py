ab53fb5c21e95171ce2737162f761c77d5db2cc5
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2025-05-14 18:19:59 +0400

    Update file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -1,15 +1,16 @@
 import os
 
 
-def check_files_exist(paths: list) -> None:
-    """
-    Checks whether the specified files exist.
+class FileManager:
+    @staticmethod
+    def check_files_exist(paths: list) -> None:
+        """
+        Checks whether the specified files exist.
+        :param paths: List of file paths to check.
+        :raises SystemExit: If any file is not found.
+        """
 
-    :param paths: List of file paths to check.
-    :raises SystemExit: If any file is not found.
-    """
-
-    for path in paths:
-        if not os.path.exists(path):
-            print(f"[!] Не найден файл: {path}")
-            exit(1)
+        for path in paths:
+            if not os.path.exists(path):
+                print(f"[!] Не найден файл: {path}")
+                exit(1)
