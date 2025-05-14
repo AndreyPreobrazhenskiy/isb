d11ec28774242d44ebf520d4bd0cfaafdc960b6d
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2025-05-14 18:20:21 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
-from utils import load_settings
-from utils import parse_args
-from file_manager import check_files_exist
-from asymmetric_encryption import generate_keys
-from symmetric_encryption import encrypt_file
-from symmetric_encryption import decrypt_file
+from utils import Utils
+from file_manager import FileManager
+from asymmetric_encryption import AsymetricEncryption
+from symmetric_encryption import SymmetricEncryption
 
 
 def main() -> None:
-    args = parse_args()
-    settings = load_settings()
+    args = Utils.parse_args()
+    settings = Utils.load_settings()
 
     if args.generation:
         print("[*] Генерация ключей...")
-        generate_keys(settings)
+        AsymetricEncryption.generate_keys(settings)
         print("[+] Ключи успешно сгенерированы.")
 
     elif args.encryption:
         print("[*] Подготовка к шифрованию...")
-        check_files_exist([settings['symmetric_key'], settings['secret_key'], settings['initial_file']])
-        encrypt_file(settings)
+        FileManager.check_files_exist([settings['symmetric_key'], settings['secret_key'], settings['initial_file']])
+        SymmetricEncryption.encrypt_file(settings)
 
     elif args.decryption:
         print("[*] Подготовка к дешифрованию...")
-        check_files_exist([settings['symmetric_key'], settings['secret_key'], settings['encrypted_file']])
-        decrypt_file(settings)
+        FileManager.check_files_exist([settings['symmetric_key'], settings['secret_key'], settings['encrypted_file']])
+        SymmetricEncryption.decrypt_file(settings)
 
 
 if __name__ == "__main__":
