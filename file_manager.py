d250216c052c39e1c111c0d6a8a3dd0ea257bdfb
AndreyPreobrazhenskiy <164298315+AndreyPreobrazhenskiy@users.noreply.github.com>
2025-05-14 18:41:39 +0400

    Update file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -1,4 +1,5 @@
 import os
+from cryptography.hazmat.primitives import serialization
 
 
 class FileManager:
@@ -14,3 +15,21 @@ class FileManager:
             if not os.path.exists(path):
                 print(f"[!] Не найден файл: {path}")
                 exit(1)
+
+
+    @staticmethod
+    def save_public_key(public_key) -> None:
+        with open('public_key.txt', 'wb') as f:
+            f.write(public_key.public_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PublicFormat.SubjectPublicKeyInfo
+            ))
+
+    @staticmethod
+    def save_private_key(private_key) -> None:
+        with open('private_key.txt', 'wb') as f:
+            f.write(private_key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption()
+            ))
